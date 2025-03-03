60f48a8b075ba5d1fe3bc60b1e1f166465acd010
Sricharan Reddy Varra <srivarra@stanford.edu>
2025-03-02 22:11:25 -0800

    ⬆️ Upgrade Narwhals (#47)

--- src/annsel/core/expr.py
+++ src/annsel/core/expr.py
@@ -40,7 +40,7 @@ class ColProtocol(Protocol):
         ...
 
 
-class Col:
+class Col(ColProtocol):
     """A wrapper around the `narwhals.col` function."""
 
     names: Iterable[str]
@@ -62,9 +62,7 @@ class Col:
         def column_selector(plx: Any):
             return plx.col(*flatten(names))
 
-        return AnnselExpr(
-            column_selector, ExprMetadata(kind=ExprKind.TRANSFORM, is_order_dependent=False), *flatten(names)
-        )
+        return AnnselExpr(column_selector, ExprMetadata(ExprKind.TRANSFORM, order_dependent=False), *flatten(names))
 
 
 col: Col = Col()
